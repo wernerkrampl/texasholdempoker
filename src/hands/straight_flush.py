f2ca89a5834cfa98d3c40886a37bb16753f0c40d
wernerkrampl <krampl.werner@gmail.com>
2025-10-05 00:32:56 +0200

    Straigh_flush was implemented, together with tests. For some reason, automatic module import of src.hands stopped working, so __init__.py files were added to get it to work again.

---
+++ src/hands/straight_flush.py
@@ -0,0 +1,40 @@
+from src.hands.hand_values import Hand_values
+
+
+class Straight_flush(Hand_values):
+    def __init__(self, cards):
+        super().__init__(cards)
+        self.value = 8
+        self.highest_card = None
+        straight_counter = 0
+        for key in range(14, 1, -1):
+            if len(self.hand_table[key]) == 1:
+                straight_counter += 1
+            else:
+                straight_counter = 0
+            if straight_counter > 1 and self.hand_table[key][0].suit != self.hand_table[key + 1][0].suit:
+                return
+            if (key == 2 and straight_counter == 4 and len(self.hand_table[14]) == 1 and self.hand_table[2][0].suit ==
+                    self.hand_table[14][0].suit):  # case for low-ace (bicycle)
+                self.highest_card = self.hand_table[5][0]
+                return
+            if straight_counter == 5:
+                self.highest_card = self.hand_table[key + 4][0]
+                break
+        return
+
+    def __eq__(self, other):
+        if not isinstance(other, Hand_values):
+            return False
+        if self.highest_card.rank == other.highest_card.rank:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        if not isinstance(other, Hand_values):
+            return False
+        if self.highest_card.rank < other.highest_card.rank:
+            return True
+        else:
+            return False
---
+++ test/hands/test_straight_flush.py
@@ -0,0 +1,31 @@
+import unittest
+
+from src.card import Card
+from src.hands.hand_table_generator import Hand_table_generator
+from src.hands.straight_flush import Straight_flush
+
+
+class TestStraight_flush(unittest.TestCase):
+    def setUp(self):
+        self.hand_1 = Straight_flush(Hand_table_generator.generate_hand_table([Card('Clubs',4),\
+                            Card('Clubs',3),\
+                            Card('Clubs',5),\
+                            Card('Clubs',2),\
+                            Card('Clubs',14)]))
+
+        self.hand_2 = Straight_flush(Hand_table_generator.generate_hand_table([Card('Diamonds', 13),\
+                            Card('Diamonds', 11),\
+                            Card('Diamonds', 10),\
+                            Card('Diamonds', 12),\
+                            Card('Diamonds', 14)]))
+
+        self.hand_3 = Straight_flush(Hand_table_generator.generate_hand_table([Card('Spades', 3), \
+                            Card('Spades', 4), \
+                            Card('Spades', 5), \
+                            Card('Spades', 2), \
+                            Card('Spades', 14)]))
+
+    def test_ordering(self):
+        assert self.hand_1 == self.hand_3
+        assert self.hand_1 < self.hand_2
+        assert self.hand_2 > self.hand_3
